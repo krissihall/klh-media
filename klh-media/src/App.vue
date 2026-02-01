25d51a7bdaa412f0a00153966c3fc646ad02e17b
Krissi Hall <krissihall@Krissis-MacBook-Pro.local>
2026-02-01 01:09:00 -0500

    master-new: created new Vue project with Bootstrap

---
+++ klh-media/src/App.vue
@@ -0,0 +1,14 @@
+<template>
+  <SplashPage />
+</template>
+
+<script>
+import SplashPage from './components/SplashPage.vue'
+
+export default {
+  name: 'App',
+  components: {
+    SplashPage
+  }
+}
+</script>
---
+++ klh-media/src/components/SplashPage.vue
@@ -0,0 +1,3 @@
+<template>
+  <div class="logo">[logo]</div>
+</template>
